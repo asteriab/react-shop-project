744a33844053d7c697e4d738f23fa1f158e97963
Mariana <mariana.balanchuk@gmail.com>
2023-02-05 22:45:03 +0100

    added react component, react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// without jsx
-// const title = React.createElement("h1", { id: "title" }, "Hello React.js");
-// console.log(title);
+function Title() {
+  return <h1>Hello TS</h1>;
+}
 
-// jsx -> js extended
-// tsx -> ts extended
-
-const title = <h1 id="title">Hello React2</h1>;
-console.log(title);
-
-const content = (
-  <div>
-    {title}
-    {10 + 10}
-    <ul className="list">
-      <li>list item1</li>
-      <li>list item2</li>
-      <li>list item3</li>
-    </ul>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ut
-      debitis, ipsa aperiam accusamus repellendus molestias, nesciunt error
-      quibusdam unde explicabo necessitatibus illum? Praesentium sint voluptatem
-      itaque fugit sit unde.
-    </p>
-  </div>
-);
-
+function List() {
+  return (
+    <React.Fragment>
+      <ul>
+        <li>list item</li>
+        <li>list item</li>
+        <li>list item</li>
+      </ul>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, commodi
+        tempora rem dolore saepe voluptatem mollitia ab! Tenetur tempora
+        dolorem, ex iusto itaque totam neque perspiciatis eligendi, id quos
+        unde!
+      </p>
+    </React.Fragment>
+  );
+}
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode>{content}</React.StrictMode>);
+
+const App = () => {
+  return (
+    <div className="App">
+      <Title />
+      <List />
+    </div>
+  );
+};
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
