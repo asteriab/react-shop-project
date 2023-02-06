5bffd4ad94b4ea46de9d0bfe025dc6d413eff6eb
Mariana <mariana.balanchuk@gmail.com>
2023-02-06 01:22:06 +0100

    dynamic style classes

--- src/List.tsx
+++ src/List.tsx
@@ -5,7 +5,7 @@ export const List = (props: Props) => {
   let data = true;
   return (
     <>
-      <ul className="content">
+      <ul className="content descr">
         <li>list item1</li>
         <li>list item2</li>
         <li>list item3</li>
@@ -17,6 +17,13 @@ export const List = (props: Props) => {
           padding: "15px",
         }}
       >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, commodi
+        tempora rem dolore saepe voluptatem mollitia ab! Tenetur tempora
+        dolorem, ex iusto itaque totam neque perspiciatis eligendi, id quos
+        unde!
+      </p>
+
+      <p className={` descr ${data ? "content" : "test"}`}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, commodi
         tempora rem dolore saepe voluptatem mollitia ab! Tenetur tempora
         dolorem, ex iusto itaque totam neque perspiciatis eligendi, id quos
