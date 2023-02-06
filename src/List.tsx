30bab6092dd1d8a9bc28213f8463bfe8535eb035
Mariana <mariana.balanchuk@gmail.com>
2023-02-06 01:02:35 +0100

    attribute styles

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,6 @@
 type Props = {};
 export const List = (props: Props) => {
+  let data = true;
   return (
     <>
       <ul>
@@ -7,7 +8,13 @@ export const List = (props: Props) => {
         <li>list item2</li>
         <li>list item3</li>
       </ul>
-      <p>
+      <p
+        style={{
+          color: "white",
+          backgroundColor: data ? "purple" : "yellow",
+          padding: "15px",
+        }}
+      >
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, commodi
         tempora rem dolore saepe voluptatem mollitia ab! Tenetur tempora
         dolorem, ex iusto itaque totam neque perspiciatis eligendi, id quos
