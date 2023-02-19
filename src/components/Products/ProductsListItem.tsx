1232c3eff0a02de6729ebae15357ba940275d7ae
Mariana <mariana.balanchuk@gmail.com>
2023-02-19 09:53:40 +0100

    added change color functionality

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -25,6 +25,7 @@ const ProductsListItem = ({
     image,
 }: Props) => {
     const [count, setCount] = useState<number>(1)
+    const [color, setColor] = useState<string>('green')
 
     const onIncrement = (num: number) => {
         setCount((prevState) => prevState + num)
@@ -45,6 +46,21 @@ const ProductsListItem = ({
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity}Gb</div>
                 <div className="product-price">${price}</div>
+
+                <div>
+                    Color:
+                    <span className={color}> {color}</span>
+                </div>
+                <button
+                    onClick={() =>
+                        setColor((prevState) =>
+                            prevState === 'green' ? 'red' : 'green'
+                        )
+                    }
+                >
+                    Change Color
+                </button>
+
                 <div className="product-quantity">
                     <Button
                         variant="outlined"
