f05480d13b9faf2b4c087c598fa661bd4ccd37e8
Mariana <mariana.balanchuk@gmail.com>
2023-02-28 22:10:52 +0100

    fixed NaN after adding other products

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -20,9 +20,22 @@ const App = (props: Props) => {
     const addProductToCart = (id: number, count: number) => {
         setProductsInCart((prevState) => ({
             ...prevState,
-            [id]: prevState[id] + count,
+            [id]: (prevState[id] || 0) + count,
         }))
     }
+
+    // const addProductToCart = (id: number, count: number) => {
+    //     setProductsInCart((prevState) => {
+    //         if (prevState[id] === undefined) {
+    //             return { ...prevState, [id]: 0 + count }
+    //         } else {
+    //             return {
+    //                 ...prevState,
+    //                 [id]: prevState[id] + count,
+    //             }
+    //         }
+    //     })
+    // }
 
     // const addProductToCart = (id: number, count: number) => {
     //     setProductsInCart((prevState) =>
