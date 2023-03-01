bc0077632434da4a57381bdb18dab10510668bef
Mariana <mariana.balanchuk@gmail.com>
2023-03-01 01:21:18 +0100

    added router removed main, added cart+home navlink

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,9 +1,12 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
-import Main from 'container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
+import { Container } from '@mui/system'
+import Home from 'pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
+import CartPage from 'pages/CartPage/CartPage'
 
 type Props = {}
 type ProductsInCartType = {
@@ -23,7 +26,15 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <Main addProductToCart={addProductToCart} />
+            <Container sx={{ padding: '60px 0' }}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Container>
             <Footer />
         </StyledEngineProvider>
     )
---
+++ src/pages/CartPage/CartPage.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+const CartPage = (props: Props) => {
+    return (
+        <div>
+            <Typography variant="h4" component="h1">
+                Cart
+            </Typography>
+        </div>
+    )
+}
+export default CartPage
